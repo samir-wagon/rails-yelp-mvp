b11b9b7927ce89fa7769bb96ed325c5bde75e610
samir aissioui <samir.aissioui@sciencespo.fr>
2019-02-14 11:21:11 +0100

    upadate rails beyond crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show"
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  root to: "controller#action"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01 40 30 55 55',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '01 40 33 54 55',
+    category:     'italian'
+  },
+  {
+    name:         'Pitaya',
+    address:      '100 rue Oberkampf',
+    phone_number:  '01 41 32 64 56',
+    category:      'chinese'
+  },
+  {
+    name:         'Esprit Chai',
+    address:      '102 rue Oberkampf',
+    phone_number: '01 40 35 89 67',
+    category:     'french'
+  },
+  {
+    name:         'Franprix',
+    address:      '90 rue Oberkampf',
+    phone_number: '01 47 40 10 21',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished, we have #{Restaurant.count} restaurants in our list!"
